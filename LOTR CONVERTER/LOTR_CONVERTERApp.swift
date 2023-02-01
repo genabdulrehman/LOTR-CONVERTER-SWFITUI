966c06ca341036106dce05c5bbe105725434f154
genabdulrehman <76816147+genabdulrehman@users.noreply.github.com>
2023-02-01 22:16:29 +0500

    Initial Commit

---
+++ LOTR CONVERTER/LOTR_CONVERTERApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_CONVERTERApp.swift
+//  LOTR CONVERTER
+//
+//  Created by Abdul Rehman on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_CONVERTERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
